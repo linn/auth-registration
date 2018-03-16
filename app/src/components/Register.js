52397abe93aefbef9afad462f3366445fbeb5858
Sandy Cormie <sandy.cormie@linn.co.uk>
2018-03-16 19:26:03 +0000

    Split register into login and template

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput, Button, ControlGroup, Header, Container, ErrorMessage, Processing } from './common';
+import Template from './templates/Register';
 import isEmail from 'validator/lib/isEmail';
 import { register } from '../actions';
 
@@ -19,63 +19,18 @@ class Register extends Component {
     }
 
     render() {
-        document.title = 'Register for a Linn account | Linn';
-
-        const { processing, email, password, password2, errors } = this.state;
-
-        return (
-            <Container>
-                <Header caption="Register" />
-                <form onSubmit={e => this.handleSubmit(e)}>
-                    {errors.registration && <ErrorMessage message={errors.registration} />}
-                    <ControlGroup>
-                        <TextInput
-                            autofocus={true}
-                            type="email"
-                            name="username"
-                            caption="Email Address"
-                            placeholder="Your email address"
-                            value={email}
-                            onChange={val => this.setState({ email: val })}
-                            error={errors.email}
-                            disabled={processing} />
-                    </ControlGroup>
-                    <ControlGroup>
-                        <TextInput
-                            type="password"
-                            name="password"
-                            caption="Password"
-                            placeholder="Your password"
-                            value={password}
-                            onChange={val => this.setState({ password: val })}
-                            error={errors.password}
-                            disabled={processing} />
-                    </ControlGroup>
-                    <ControlGroup>
-                        <TextInput
-                            type="password"
-                            name="password2"
-                            caption="Repeat Password"
-                            placeholder="Repeat your password"
-                            value={password2}
-                            onChange={val => this.setState({ password2: val })}
-                            error={errors.password2}
-                            disabled={processing} />
-                    </ControlGroup>
-                    <ControlGroup padding="30px">
-                        {processing
-                            ? <Processing />
-                            : <Button caption="Register" type="submit" disabled={processing} />
-                        }
-                    </ControlGroup>
-                </form>
-            </Container>
-        );
+        return <Template
+            {...this.state}
+            onSubmit={e => this.handleSubmit(e)}
+            onEmailChange={val => this.setState({ email: val })}
+            onPasswordChange={val => this.setState({ password: val })}
+            onPassword2Change={val => this.setState({ password2: val })}
+        />
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        const {history} = this.props;
+        const { history } = this.props;
 
         if (this.validate()) {
             this.setState({ processing: true });
@@ -105,29 +60,31 @@ class Register extends Component {
             registration: ''
         }
 
-        if (!this.state.email) {
+        const { email, password, password2 } = this.state;
+
+        if (!email) {
             errors.email = 'You must specify your email address';
             valid = false;
         }
-        else if (!isEmail(this.state.email)) {
+        else if (!isEmail(email)) {
             errors.email = 'This doesn\'t seem to be a valid email address';
             valid = false;
         }
 
-        if (!this.state.password) {
+        if (!password) {
             errors.password = 'You must specify a password';
             valid = false;
         }
-        else if (this.state.password.length < 8) {
+        else if (password.length < 8) {
             errors.password = 'Your password must have at least 8 characters';
             valid = false;
         }
 
-        if (!this.state.password2) {
+        if (!password2) {
             errors.password2 = 'You must repeat your password';
             valid = false;
         }
-        else if (this.state.password && this.state.password !== this.state.password2) {
+        else if (password && password !== password2) {
             errors.password2 = 'The passwords entered don\'t match';
             valid = false;
         }
